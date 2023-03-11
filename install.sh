efd0c7a33973e902481532a98cdb7d0477b7e9e5
Thorpy <tom_thorpe2004@hotmail.com>
2023-03-11 18:21:15 +0000

    Update install.sh
    updated to use env variables instead of hardcoding the user as "pi"

--- install.sh
+++ install.sh
@@ -4,20 +4,23 @@
 apt-get update
 apt-get install -y git python3-pip screen
 
-# Clone the BioBlitz repository to /home/pi/
-cd /home/pi/
+# Get the home directory of the current user
+home_dir=$HOME
+
+# Clone the BioBlitz repository to the user's home directory
+cd $home_dir
 git clone https://github.com/Thorpy/BioBlitz
 
 # Install requirements for main.py
-cd /home/pi/BioBlitz/bioblitz-game
+cd $home_dir/BioBlitz/bioblitz-game
 pip3 install -r requirements.txt
 
 # Run setup.py script
-cd /home/pi/BioBlitz//raspi-captive-portal
-yes Y | sudo python /home/pi/BioBlitz/raspi-captive-portal/setup.py
+cd $home_dir/BioBlitz/raspi-captive-portal
+yes Y | sudo python $home_dir/BioBlitz/raspi-captive-portal/setup.py
 
 # Add startup command to rc.local
-sed -i -e '$i su -c "screen -dmS main /usr/bin/python3 /home/pi/BioBlitz/bioblitz-game/main.py" pi\n' /etc/rc.local
+sed -i -e '$i su -c "screen -dmS main /usr/bin/python3 $home_dir/BioBlitz/bioblitz-game/main.py" $USER\n' /etc/rc.local
 
 # Start the main.py script in a new screen session
-screen -dmS main /usr/bin/python3 /home/pi/BioBlitz/bioblitz-game/main.py
+screen -dmS main /usr/bin/python3 $home_dir/BioBlitz/bioblitz-game/main.py
