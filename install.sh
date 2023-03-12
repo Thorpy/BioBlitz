7fda15f84cadf83878670967a6c3a2fecd13de38
Thorpy <tom_thorpe2004@hotmail.com>
2023-03-12 18:56:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,6 @@ user=$SUDO_USER
 cd $home_dir
 git clone https://github.com/Thorpy/BioBlitz
 
-# Change the owner of the BioBlitz directory to the user
-chown -R $user:$user $home_dir/BioBlitz
-
 # Install requirements for main.py
 cd $home_dir/BioBlitz/bioblitz-game
 pip3 install -r requirements.txt
@@ -28,5 +25,9 @@ sudo sed -i '/exit 0/d' /etc/rc.local
 sudo sh -c "echo 'su $user -c \"screen -dmS bioblitz python3 $home_dir/BioBlitz/bioblitz-game/main.py\"' >> /etc/rc.local"
 sudo sh -c "echo 'exit 0' >> /etc/rc.local"
 
+# Change the owner of the BioBlitz directory to the user
+chown -R $user:$user $home_dir/BioBlitz
+
 echo "Installation complete. The system will now reboot."
+
 reboot
